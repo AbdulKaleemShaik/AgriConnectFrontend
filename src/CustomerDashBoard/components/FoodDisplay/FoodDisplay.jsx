03112b539290e7fae09e14f8571bcbcb49392e81
Abdul Kaleem Shaik <abdulkaleem8360@gmail.com>
2024-10-04 08:28:46 +0530

    Added refresh functionlaity

--- src/CustomerDashBoard/components/FoodDisplay/FoodDisplay.jsx
+++ src/CustomerDashBoard/components/FoodDisplay/FoodDisplay.jsx
@@ -1,18 +1,25 @@
 import React, { useContext } from "react";
 import "./FoodDisplay.css";
 import { StoreContext } from "../../../Context/StoreContext";
+import { useEffect } from "react";
+import axiosInstance from "../../../Service/AxiosInstant";
 import FoodItem from "../FoodItem/FoodItem";
 const FoodDisplay = ({ category }) => {
-  const { food_list_data } = useContext(StoreContext);
+  const { food_list_data, setFoodListData } = useContext(StoreContext);
   //console.log(category);
   // console.log(food_list_data);
+  useEffect(() => {
 
+    axiosInstance.get('/allproducts').then((response) => {
+      setFoodListData(response.data);
+    });
+  }, []);
 
   return (
     <div className="food-display" id="food-display">
       <h2>Top Fresh groceries near you</h2>
       <div className="food-display-list">
-        { food_list_data &&food_list_data.map((item, index) => {
+        {food_list_data && food_list_data.map((item, index) => {
           if (category === "All" || category === item.category) {
             // console.log(item.id);
             return (
