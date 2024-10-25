4c2c84bac243db26d7d71c2a29e11a0a921afcba
Naveen Kumar Reddy <bnkreddy7571@gmail.com>
2024-10-25 09:53:05 +0530

    updated

--- src/CustomerDashBoard/CustomerDashBoardLayout.jsx
+++ src/CustomerDashBoard/CustomerDashBoardLayout.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
-// import "./components/Navbar/Navbar.module.css";
+import "./components/Navbar/Navbar";
 
 
 const CustomerDashboardLayout = () => {
+
     return (
+        <>
         <div>
             <main>
                 <Outlet /> {/* Renders the child routes */}
             </main>
         </div>
+        </>
     );
 };
 
